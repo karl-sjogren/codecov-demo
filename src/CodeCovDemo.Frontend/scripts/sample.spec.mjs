036f3753b49b541314c5abaf6941e8f6004d202f
Karl-Johan Sjögren <karl-johan.sjogren@xlent.se>
2025-02-20 13:47:21 +0100

    Removed some stuff

--- src/CodeCovDemo.Frontend/scripts/sample.spec.mjs
+++ src/CodeCovDemo.Frontend/scripts/sample.spec.mjs
@@ -6,12 +6,4 @@ describe('sample', () => {
     expect(sum(1, 2)).toBe(3);
   });
 
-  test('subtracts 2 - 1 to equal 1', () => {
-    expect(subtract(2, 1)).toBe(1);
-  });
-
-  test('divides 6 / 2 to equal 3', () => {
-    expect(divide(6, 2)).toBe(3);
-  });
-
 });
